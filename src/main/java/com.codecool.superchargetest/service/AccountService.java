653ee420d5e4d66d6b2b76a4ab0e3267ac8c7af6
adamGrob <ad4mgr0b@gmail.com>
2018-09-07 16:13:31 +0200

    Created the changeBalance and transfer functions. Transfer can transfer the amount from one table to the next, changebalance is an universal method which can be used for balance changing operations.

--- src/main/java/com.codecool.superchargetest/service/AccountService.java
+++ src/main/java/com.codecool.superchargetest/service/AccountService.java
@@ -1,26 +1,49 @@
 package com.codecool.superchargetest.service;
 
 import com.codecool.superchargetest.ejb.Account;
+import com.codecool.superchargetest.ejb.Transaction;
+import com.codecool.superchargetest.model.TransActionType;
 import com.codecool.superchargetest.repository.AccountRepository;
+import com.codecool.superchargetest.repository.TransActionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
+import java.util.Date;
 
 @Service
 public class AccountService {
 
     private AccountRepository accountRepository;
 
+    private TransActionRepository transActionRepository;
+
     @Autowired
-    public AccountService(AccountRepository accountRepository) {
+    public AccountService(AccountRepository accountRepository, TransActionRepository transActionRepository) {
         this.accountRepository = accountRepository;
+        this.transActionRepository = transActionRepository;
     }
 
     public void transfer(Account fromAccount, Account toAccount, BigDecimal amount) {
-        accountRepository.changeBalance(fromAccount.getId(), amount.negate());
-        accountRepository.changeBalance(toAccount.getId(), amount);
+        changeBalance(fromAccount, amount, TransActionType.WITHDRAW);
+
+        changeBalance(toAccount, amount, TransActionType.DEPOSIT);
     }
 
+    public void changeBalance(Account account, BigDecimal amount, TransActionType transActionType){
+        Transaction transActionB = new Transaction();
+        transActionB.setBalanceAtTransAction(account.getBalance());
+        transActionB.setDate(new Date());
+        transActionB.setTransActionType(transActionType);
+        transActionB.setAccount(account);
+        if(transActionType.equals(TransActionType.DEPOSIT)) {
+            transActionB.setTransactionAmount(amount);
+            accountRepository.changeBalance(account.getId(), amount);
+        } else {
+            transActionB.setTransactionAmount(amount.negate());
+            accountRepository.changeBalance(account.getId(), amount.negate());
+        }
+        transActionRepository.save(transActionB);
+    }
 }
 
 
